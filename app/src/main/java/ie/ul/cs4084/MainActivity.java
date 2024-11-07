11586820016c0efd9d292a2474f60b7dd5f7bae9
KlaudiaP09 <piotr.regalis@gmail.com>
2024-11-07 15:37:05 +0000

    [SCRUM 3] added swipe main page
    added main page with swipe functionality

--- app/src/main/java/ie/ul/cs4084/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084/MainActivity.java
@@ -7,10 +7,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.widget.Toast;
 import android.util.Log;
 import java.util.ArrayList;
 
+
+import com.google.android.material.tabs.TabLayout;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment1(), "Main");
+        vpAdapter.addFragment(new fragment2(), "Chat");
+        vpAdapter.addFragment(new fragment3(), "Map");
+        viewPager.setAdapter(vpAdapter);
     }
+
+
+        }
