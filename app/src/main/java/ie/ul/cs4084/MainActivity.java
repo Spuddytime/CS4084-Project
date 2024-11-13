6ab726b7eff7eb11d69a3b484d7a196c4cd7ec6f
EV1912 <elliotvowles737@gmail.com>
2024-11-13 15:07:49 +0000

    [Scrum-12]

--- app/src/main/java/ie/ul/cs4084/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
+        });
+        button.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Toast Test",Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
