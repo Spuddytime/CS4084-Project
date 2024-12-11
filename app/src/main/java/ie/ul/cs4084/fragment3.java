4408765cda8688707ec016ddc4540e08f16556b8
EV1912 <elliotvowles737@gmail.com>
2024-12-11 14:18:29 +0000

    Adding a back button
    Heres a back button for Google maps, let me know if it works

--- app/src/main/java/ie/ul/cs4084/fragment3.java
+++ app/src/main/java/ie/ul/cs4084/fragment3.java
@@ -1,14 +1,18 @@
 package ie.ul.cs4084;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,6 +30,9 @@ public class fragment3 extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        //adding navigation from maps back to the app
+        toolbar.setNavigationOnClickListener(v -> { requireActivity().onBackPressed();});
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
