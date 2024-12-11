d148f82483fcac403fb7d213dc44c706579aa054
EV1912 <elliotvowles737@gmail.com>
2024-12-11 11:32:16 +0000

    another update
    Another update for the maps function, let me know if this works

--- app/src/main/java/ie/ul/cs4084/fragment3.java
+++ app/src/main/java/ie/ul/cs4084/fragment3.java
@@ -1,15 +1,14 @@
 package ie.ul.cs4084;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,29 +16,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class fragment3 extends Fragment implements OnMapReadyCallback {
-GoogleMap map;
+    GoogleMap map;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false);
-    }
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view,savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if(mapFragment != null){
+        if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+        return view;
     }
 
-
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap){
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-        LatLng Cork = new LatLng(51.8985,8.4756);
+        LatLng Cork = new LatLng(51.8985, 8.4756);
         map.addMarker(new MarkerOptions().position(Cork).title("Cork"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Cork));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Cork,10));
     }
 }
